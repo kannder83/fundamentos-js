d594067dc1201cd804196276c5e481610d32746b
Alejandro Ruiz <kannder.ziur@gmail.com>
2020-11-17 09:58:33 -0500

    Se crea la clase 07.

--- scripts/clase06.js
+++ scripts/clase06.js
@@ -1,1 +1,43 @@
-//Objetos
+//Clasr de Objetos
+let nombre = "Alejandro";
+const parrafo = "paragraph";
+
+//Función
+function imprimirNombreEnMayusculas(nombre) {
+  nombre = nombre.toUpperCase();
+  document.getElementById(parrafo).innerHTML += `${nombre} <br>`;
+  console.log(nombre);
+}
+
+imprimirNombreEnMayusculas(nombre);
+
+//Se crea el objeto:
+
+let sacha = {
+  nombre: "Sacha",
+  apellido: "Lifszyc",
+  edad: 28,
+};
+
+//Para imprimir el valor:
+
+imprimirNombreEnMayusculas(sacha.nombre);
+
+// Otra forma es utilizar el objeto completo, sin necesidad de acceder al atributo.
+
+function imprimirNombreEnMayusculaObjeto(persona) {
+  let nombre = persona.nombre.toUpperCase();
+  document.getElementById(parrafo).innerHTML += `${nombre} <br>`;
+}
+
+imprimirNombreEnMayusculaObjeto(sacha);
+
+//Se puede traer solo el atributo.
+// No declarar 2 veces la variable.
+
+function imprimirNombreEnMayusculaAtributo({ nombre }) {
+  let name = nombre.toUpperCase();
+  document.getElementById(parrafo).innerHTML += `${name} <br>`;
+}
+
+imprimirNombreEnMayusculaAtributo(sacha);
---
+++ scripts/clase07.js
@@ -0,0 +1,1 @@
+// Desestructurar objetos

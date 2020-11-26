000b0d952c388cb89450aeb5675ab1030956bb96
Alejandro Ruiz <kannder.ziur@gmail.com>
2020-11-26 17:36:35 -0500

    Enviar función en una función.

--- scripts/clase21.js
+++ scripts/clase21.js
@@ -11,9 +11,12 @@ class Persona {
     (this.nombre = nombre), (this.apellido = apellido), (this.altura = altura);
   }
   //Se agrega Metodo Saludar:
-  saludar() {
+  saludar(fn) {
     console.log(`Hola, me llamo ${this.nombre} ${this.apellido}.`);
     escribirHtml(`Hola, me llamo ${this.nombre} ${this.apellido}.`);
+    if (fn) {
+      fn(this.nombre, this.apellido, false);
+    }
   }
   //Se agrega el Metodo soyAlto
   soyAlto() {
@@ -26,27 +29,35 @@ class Desarrollador extends Persona {
   constructor(nombre, apellido, altura) {
     super(nombre, apellido, altura);
   }
-  //Se crea el metodo Saludar
-  saludar() {
+  //Se crea el metodo Saludar, incluye funcion como método.
+  saludar(fn) {
     escribirHtml(
       `Hola me llamo ${this.nombre} ${this.apellido} y soy desarrollador.`
     );
     console.log(
       `Hola me llamo ${this.nombre} ${this.apellido} y soy desarrollador.`
     );
+    //Se agrega la respueta a la funcion:
+    if (fn) {
+      fn(this.nombre, this.apellido, true);
+    }
   }
 }
 
-//Se crean los objetos de Persona:
+function responderSaludo(nombre, apellido, esDev) {
+  console.log(`Buen día ${nombre} ${apellido}`);
+  escribirHtml(`Buen día ${nombre} ${apellido}`);
+  if (esDev) {
+    console.log(`Ah mirá, no sabia que eras desarrollador/a.`);
+    escribirHtml(`Ah mirá, no sabia que eras desarrollador/a.`);
+  }
+}
+
 let alejandro = new Persona("Alejandro", "Ruiz", 1.7);
-let fernanda = new Persona("Fernanda", "Davila", 1.9);
+let daniela = new Persona("Daniela", "Perez", 1.5);
+let thomas = new Desarrollador("Thomas", "Solano", 1.7);
 
-//Se crean los objetos de Desarrollador:
-let diego = new Desarrollador("Diego", "Ortiz", 1.84);
-
-//Se llaman los objetos de Persona:
+//Se incluye la respuesta como parametro de la función:
 alejandro.saludar();
-alejandro.soyAlto();
-
-//Se llama la funcion del prototipo:
-diego.saludar();
+daniela.saludar(responderSaludo);
+thomas.saludar(responderSaludo);
